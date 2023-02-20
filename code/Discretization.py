51b037fff5c6ab51ef6f68b093a8921e9e56e043
Nikhil Mehra <nmehra2@ncsu.edu>
2023-02-20 17:16:17 -0500

    Implemented mergeAny()

--- code/Discretization.py
+++ code/Discretization.py
@@ -1,3 +1,4 @@
+import math
 from copy import deepcopy
 from Sym import Sym
 
@@ -19,3 +20,26 @@ def merge2(col1, col2):
     new = merge(col1, col2)
     if new.div() <= (col1.div() * col1.n + col2.div() * col2.n) / new.n:
         return new
+
+
+def mergeAny(ranges0):
+    def noGaps(t):
+        for i in range(1, len(t)):
+            t[i].lo = t[i - 1].hi
+        t[0].lo = -math.inf
+        t[-1].hi = math.inf
+        return t
+
+    j = 0
+    ranges1 = []
+    while j < len(ranges0):
+        left, right = ranges0[j], ranges0[j+1]
+        if right:
+            y = merge2(left.y, right.y)
+            if y:
+                j += 1
+                left.hi, left.y = right.hi, y
+        ranges1.append(left)
+        j += 1
+
+    return noGaps(ranges0) if len(ranges0) == len(ranges1) else mergeAny(ranges1)
